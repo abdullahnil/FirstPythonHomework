001fa5e8c24a56563b294a2bea72ec71fcf62e05
Abdullah Nil <abdullahnil@hotmail.com>
2024-10-22 01:14:28 +0300

    Update hhw.py

--- hhw.py
+++ hhw.py
@@ -1,14 +1,13 @@
 import math
-import stdio
 
 
 while True:
     x = float(input('This is homework from Abdullah Nil \n Please input any number for take square root: '))
     if x < 0:
-        stdio.writeln('You should not use the negative numbers')
-        stdio.writeln('Please try again with a positive number')
+        print('You should not use the negative numbers')
+        print('Please try again with a positive number')
 
     else:
-        stdio.writeln('Taking the square root of the given expression...')
-        stdio.writeln(math.sqrt(x))
+        print('Taking the square root of the given expression...')
+        print(math.sqrt(x))
         break
